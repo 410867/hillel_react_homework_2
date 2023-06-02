2cd470d427665d75b212859cc108b2013e541e0d
410867 <oshchyp123@gmail.com>
2023-06-02 15:34:46 +0300

    Adding App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+          name: 'Stepan',
+          age: 25,
+          isToggle: true,
+          isToggleVisible: true,
+      };
+  }
+
+  toggleHandler = () => {
+      this.state.isToggle ?
+          this.setState({name: 'Mykola', age: 30, isToggle: false}) :
+          this.setState({name: 'Stepan', age: 25, isToggle: true});
+  }
+
+    toggleVisibility = () => {
+      this.state.isToggleVisible ? this.setState({isToggleVisible: false}) : this.setState({isToggleVisible: true});
+    }
+
+  render() {
+    return (
+        <div className="index">
+          <div>
+              <p className={this.state.isToggleVisible ? '' : 'hide'}>
+                  Name: {this.state.name}, age: {this.state.age}
+              </p>
+              <button onClick={this.toggleHandler}>Click on me</button>
+              <button onClick={this.toggleVisibility}>
+                  {this.state.isToggleVisible ? 'Скрыть' : 'Показать'}
+              </button>
+          </div>
+        </div>
+    );
+  }
 }
+
 
 export default App;
